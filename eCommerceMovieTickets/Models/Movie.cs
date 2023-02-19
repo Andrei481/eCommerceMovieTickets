2ac209f24218bcef1a65c30ea1cf153a00384ec7
Andrei J <andrei.joldea@student.upt.ro>
2023-02-19 14:45:33 +0200

    Added cinema, movie, producer models

---
+++ eCommerceMovieTickets/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using eCommerceMovieTickets.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace eCommerceMovieTickets.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public double Price { get; set; }
+        public Cinema CinemaName { get; set; }
+        public Actor Actor { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+        public Producer Producer { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
